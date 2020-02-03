97bd835ca1633bdea585ba72387422d936e7ea66
uqngibbo <uqngibbo@uq.edu.au>
2020-02-03 18:25:39 +1000

    Increased the amount of spam and added another pointless function

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ A pointless virtual machine
 """
 
 from library import print_message, replace_words_with_spam
-from variables import HELLO
+from variables import HELLO, POWER
 
-print_message(replace_words_with_spam(HELLO))
+print_message(replace_words_with_spam(POWER))
